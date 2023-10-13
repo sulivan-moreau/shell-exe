b807e1eb3addbaa0b1ec1a7b336bf0b8e072f64e
System Administrator <root@MacBook-Air-de-Sulivan.local>
2023-10-13 10:33:22 +0200

    Mise a jour

--- Job08/get_logs.sh
+++ Job08/get_logs.sh
@@ -2,14 +2,19 @@
 
 #Variable
 user="E0E577C7-6277-43AF-8504-D3275A1744D3"
-chemin="/Users/sulivanmoreau/shell-exe/Job08"
+chemin="/Users/sulivanmoreau/shell-exe/Job08/Backup"
 date=$(date '+%d-%m-%Y-%H:%M')
 
 #Création fichier et enregistrement dans Job08
 touch "$chemin/number_connection_$date"
 
 #Recherche et compter les logs et enregistrer dans le fichier
-nombre_connection=$(grep "$user" /var/log/system.log | wc -l)
-echo "$nombre_connection" > "$chemin/number_connection_$date"
+nombre_connection=grep "$user" /var/log/system.log | wc -l > "$chemin/number_connection_$date"
 
+#Archive en Tar 
+tar -czf "$chemin/number_connection_$date.tar.gz" "$chemin/number_connection_$date"
 
+#Suppresion fichier
+rm "$chemin/number_connection_$date" 
+
+echo "ca marche bg"
