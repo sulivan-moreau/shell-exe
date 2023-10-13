52712562e92d2e74f379394783e52f8c29252a2c
System Administrator <root@MacBook-Air-de-Sulivan.local>
2023-10-13 09:47:57 +0200

    Résolution du conflit dans hello_bye.sh

--- Job05/hello_bye.sh
+++ Job05/hello_bye.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-=======
 # Récupérer l'argument passé
 argument="$1"
 
->>>>>>> c3b81b00ff5f88cdb76ea4f8c47aaaacb4566359
 # Vérifier la valeur de l'argument et afficher le message correspondant
 if [ "$1" = "Hello" ]; then
     echo "Bonjour, je suis un script !"
